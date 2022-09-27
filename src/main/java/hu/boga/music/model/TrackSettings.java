1042bb3ac86bb712f41bc0acb6f31816b4e90fc9
kunb <balazs.kun@idomsoft.hu>
2022-09-27 09:19:28 +0200

    refaktor

--- src/main/java/hu/boga/music/model/TrackSettings.java
+++ src/main/java/hu/boga/music/model/TrackSettings.java
@@ -11,14 +11,8 @@ import hu.boga.music.enums.Tone;
 
 public class TrackSettings {
     private int program;
-//    private NoteLength length;
-//    private NoteLength gapLength;
-//    private int octave;
-//    private NoteName scaleRoot;
-//    private Tone scaleTone;
     private int midiChannel;
     private int volume;
-//    private boolean pattern;
 
     private int rgb = new Random().nextInt();
 
@@ -27,12 +21,8 @@ public class TrackSettings {
     }
 
     public TrackSettings(TrackSettings settings) {
-//        this.scaleRoot = settings.scaleRoot;
-//        this.length = settings.length;
         this.midiChannel = settings.midiChannel;
-//        this.octave = settings.octave;
         this.program = settings.program;
-//        this.scaleTone = settings.scaleTone;
         this.volume = settings.volume;
     }
 
@@ -40,30 +30,10 @@ public class TrackSettings {
         return program;
     }
 
-//    public NoteLength getLength() {
-//        return length;
-//    }
-//
-//    public int getOctave() {
-//        return octave;
-//    }
-//
-//    public NoteName getScaleRoot() {
-//        return scaleRoot;
-//    }
-//
-//    public Tone getScaleTone() {
-//        return scaleTone;
-//    }
 
     public static TrackSettings defaultSettings() {
         TrackSettings ts = new TrackSettings();
-//        ts.scaleRoot = NoteName.C;
-//        ts.length = NoteLength.HARMICKETTED;
-//        ts.gapLength = NoteLength.HARMICKETTED;
-//        ts.scaleTone = Tone.MAJ;
         ts.program = 49;
-//        ts.octave = 2;
         ts.setVolume(120);
         return ts;
     }
@@ -79,22 +49,6 @@ public class TrackSettings {
     public void setProgram(int program) {
         this.program = program;
     }
-
-//    public void setLength(NoteLength length) {
-//        this.length = length;
-//    }
-//
-//    public void setOctave(int octave) {
-//        this.octave = octave;
-//    }
-//
-//    public void setScaleRoot(NoteName scaleRoot) {
-//        this.scaleRoot = scaleRoot;
-//    }
-//
-//    public void setScaleTone(Tone scaleTone) {
-//        this.scaleTone = scaleTone;
-//    }
 
     public void setMidiChannel(int midiChannel) {
         this.midiChannel = midiChannel;
@@ -116,21 +70,5 @@ public class TrackSettings {
     public void setRgb(int rgb) {
         this.rgb = rgb;
     }
-
-//    public void setGapLength(NoteLength selectedNoteLength) {
-//        this.gapLength = selectedNoteLength;
-//    }
-//
-//    public NoteLength getGapLength() {
-//        return gapLength;
-//    }
-//
-//    public boolean isPattern() {
-//        return pattern;
-//    }
-//
-//    public void setPattern(boolean pattern) {
-//        this.pattern = pattern;
-//    }
 
 }
